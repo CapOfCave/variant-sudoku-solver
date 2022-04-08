767d9d0645b0c522564c66c81b7ceae7a89b305c
Lars Kecker <lars.kecker2009@gmail.com>
2022-04-08 19:53:31 +0200

    initialize maven project

---
+++ src/main/java/me/kecker/sudokusolver/App.java
@@ -0,0 +1,13 @@
+package me.kecker.sudokusolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/me/kecker/sudokusolver/AppTest.java
@@ -0,0 +1,20 @@
+package me.kecker.sudokusolver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
